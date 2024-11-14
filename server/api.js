b9f7e8b5a4d56a126cc1d12a539cdf4dd1358ee2
Emresen54 <neziremresen1907@gmail.com>
2024-11-14 08:48:11 +0100

    initial commit

---
+++ server/api.js
@@ -0,0 +1,26 @@
+const { body, validationResult} = require('express-validator');
+
+const initializeAPI = async (app) => {
+app.post("/api/login", body('username').notEmpty().isEmail(), 
+body('password').notEmpty().isLength({ min: 10 }), login);
+};
+
+const login = async (req, res) => {
+const errors = validationResult(req);
+if (!errors.isEmpty()) {
+return res.status(400).json({ errors: errors.array() })
+}
+
+const { username, password } = req.body;
+
+const answer = `
+<h1>Answer</h1>
+<p>Username: ${username}</p>
+<p>Password: ${password}</p>
+`;
+
+res.send(answer);
+};
+
+module.exports = { initializeAPI };
+ 
